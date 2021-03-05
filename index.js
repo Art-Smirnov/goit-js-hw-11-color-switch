155983d0061714095b55d2c4c5a12c4f8527c093
Artem Smirnov <aratatem2013@gmail>
2021-03-05 12:10:43 +0200

    виконує завдання

---
+++ index.js
@@ -0,0 +1,28 @@
+const colors = ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"];
+
+const refs = {
+  body: document.querySelector("body"),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
+refs.startBtn.addEventListener("click", onStartBtnClick);
+refs.stopBtn.addEventListener("click", onStopBtnClick);
+
+let timerId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function onStartBtnClick(e) {
+  e.currentTarget.setAttribute("disabled", true);
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+}
+
+function onStopBtnClick(e) {
+  refs.startBtn.removeAttribute("disabled");
+  clearInterval(timerId);
+}
