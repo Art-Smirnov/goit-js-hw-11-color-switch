eb81ec87c98dd9e462b69872a4267e5328870cee
Artem Smirnov <aratatem2013@gmail>
2021-03-05 19:52:45 +0200

    виконує завдання

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const colors = ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"];
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
 
 const refs = {
-  body: document.querySelector("body"),
+  body: document.querySelector('body'),
   startBtn: document.querySelector('button[data-action="start"]'),
   stopBtn: document.querySelector('button[data-action="stop"]'),
 };
 
-refs.startBtn.addEventListener("click", onStartBtnClick);
-refs.stopBtn.addEventListener("click", onStopBtnClick);
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
 
 let timerId = null;
 
@@ -16,13 +23,14 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 function onStartBtnClick(e) {
-  e.currentTarget.setAttribute("disabled", true);
+  e.currentTarget.setAttribute('disabled', true);
   timerId = setInterval(() => {
-    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    refs.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
   }, 1000);
 }
 
 function onStopBtnClick(e) {
-  refs.startBtn.removeAttribute("disabled");
+  refs.startBtn.removeAttribute('disabled');
   clearInterval(timerId);
 }
